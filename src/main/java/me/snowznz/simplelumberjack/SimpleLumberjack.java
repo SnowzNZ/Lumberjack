072506b9a749af22aaf20531f6f5378642313c5b
SnowzNZ <jackson.baylis@outlook.co.nz>
2023-06-14 21:29:03 +1200

    formatting and optimization

--- src/main/java/me/snowznz/simplelumberjack/SimpleLumberjack.java
+++ src/main/java/me/snowznz/simplelumberjack/SimpleLumberjack.java
@@ -13,26 +13,16 @@ import org.bukkit.scheduler.BukkitRunnable;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.bukkit.Bukkit.getPluginManager;
-
 public class SimpleLumberjack extends JavaPlugin implements Listener {
-    FileConfiguration config = this.getConfig();
+    private FileConfiguration config;
 
     @Override
     public void onEnable() {
-        // Setup config
+        config = getConfig();
         config.addDefault("block-break-duration", 2);
-//        config.addDefault("wooden-axe-duration", 2);
-//        config.addDefault("stone-axe-duration", 2);
-//        config.addDefault("iron-axe-duration", 2);
-//        config.addDefault("gold-axe-duration", 2);
-//        config.addDefault("diamond-axe-duration", 2);
-//        config.addDefault("netherite-axe-duration", 2);
-
         config.options().copyDefaults(true);
         saveConfig();
 
-        // Register events
         getServer().getPluginManager().registerEvents(this, this);
     }
 
@@ -42,36 +32,24 @@ public class SimpleLumberjack extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
-        // Get the block broken
         Block blockBroken = event.getBlock();
-
-        // Check if the block broken is a log
         if (isLogBlock(blockBroken.getType())) {
-            // Get a list of the blocks around the broken block
             List<Block> surroundingLogs = getSurroundingLogs(blockBroken.getLocation());
 
-            for (Block block : surroundingLogs) {
-                if (isLogBlock(block.getType())) {
-                    // Break block after 0.5 seconds (in ticks)
-                    new BukkitRunnable() {
-                        @Override
-                        public void run() {
-                            // Create a new BlockBreakEvent
-                            BlockBreakEvent breakEvent = new BlockBreakEvent(block, event.getPlayer());
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    for (Block block : surroundingLogs) {
+                        BlockBreakEvent breakEvent = new BlockBreakEvent(block, event.getPlayer());
+                        getServer().getPluginManager().callEvent(breakEvent);
 
-                            // Call the appropriate method to trigger the event
-                            getPluginManager().callEvent(breakEvent);
-
-                            // Check if the event was not cancelled
-                            if (!breakEvent.isCancelled()) {
-                                // Break the block naturally
-                                block.breakNaturally();
-                            }
+                        if (!breakEvent.isCancelled()) {
+                            event.getPlayer().sendBlockDamage(block.getLocation(), 1);
+                            block.breakNaturally();
                         }
-                    }.runTaskLater(this, config.getInt("block-break-duration"));
-
+                    }
                 }
-            }
+            }.runTaskLater(this, config.getInt("block-break-duration"));
         }
     }
 
@@ -81,22 +59,17 @@ public class SimpleLumberjack extends JavaPlugin implements Listener {
         int x = location.getBlockX();
         int y = location.getBlockY();
         int z = location.getBlockZ();
-        Location tempLocation = new Location(location.getWorld(), x, y, z);
 
-        // Iterate over the neighboring blocks within a 1-block radius
         for (int offsetX = -1; offsetX <= 1; offsetX++) {
             for (int offsetY = 0; offsetY <= 1; offsetY++) {
                 for (int offsetZ = -1; offsetZ <= 1; offsetZ++) {
-                    // Skip the central block and blocks below
-                    if ((offsetX == 0 && offsetY == 0 && offsetZ == 0)) {
+                    if (offsetX == 0 && offsetY == 0 && offsetZ == 0) {
                         continue;
                     }
 
-                    tempLocation.setX(x + offsetX);
-                    tempLocation.setY(y + offsetY);
-                    tempLocation.setZ(z + offsetZ);
-
+                    Location tempLocation = new Location(location.getWorld(), x + offsetX, y + offsetY, z + offsetZ);
                     Block surroundingBlock = tempLocation.getBlock();
+
                     if (isLogBlock(surroundingBlock.getType())) {
                         surroundingLogs.add(surroundingBlock);
                     }
